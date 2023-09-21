7c1565b75a9167923f29b69beab4d9561c504831
Corrado Cionini <corrado.cionini@protonmail.com>
2023-09-21 18:24:49 +0200

    feat: Greeting island component

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let greetings: string[]
+
+  $: greeting = greetings[0]
+
+  function changeGreeting() {
+    greeting = greetings[Math.floor(Math.random() * greetings.length)]
+  }
+</script>
+<div>
+  <h3>{greeting}! Thank you for visiting</h3>
+  <button on:click={changeGreeting}>
+    New greeting
+  </button>
+</div>
